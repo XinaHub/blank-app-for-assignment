155b914b29542aa332cc806b8231787bc3b1fa16
XinaHub <tan_aiwei@hotmail.com>
2025-08-05 00:47:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def main():
 
     small_font_sidebar = """
     <div style='font-size:12px'>
-    <b> We're a team of three from BCA’s Construction Productivity and Quality Group — two of us from the Digitalisation Department, and one from the Buildable Design team. We've worked on projects under CORENET X (CX) and are familiar with some of the common challenges that come with the process. With that experience, we're exploring how AI can help make the work smoother and less time-consuming — especially when it comes to processing and understanding complex BIM models.</b> 
+    <b> We're a team of 3 from BCA’s Construction Productivity and Quality Group (2 from the Digitalisation Dept + 1 from the Buildable Design Dept). We've worked on projects under CORENET X (CX) and encountered some of the common challenges that come with the process. As such, we're exploring how AI can help make the work smoother and less time-consuming — especially when it comes to processing and understanding complex BIM models. 
 
     <h4>🔍 Problem Statement</h4>
     The complexity of building data represented in Industry Foundation Classes (IFC) models poses challenges in accurately identifying the properties of various elements (e.g., precast walls). The use of abbreviations and shorthand notations in naming conventions contributes to confusion and misinterpretation, resulting in inefficiencies for processing officers.
