40843dcfa60777d68f07cb101070549af0d2d29b
XinaHub <tan_aiwei@hotmail.com>
2025-08-07 14:34:29 +0800

    Update Methodology.py

--- pages/Methodology.py
+++ pages/Methodology.py
@@ -4,7 +4,7 @@ from auth import check_password  # 👈 import your password checker
 if not check_password():
     st.stop()
 
-st.set_page_config(page_title="Analysis", page_icon="📊")
+st.set_page_config(page_title="Flowchart", page_icon="📊")
 
 st.title("📊 Analysis Page")
 st.write("Flowchart.")
