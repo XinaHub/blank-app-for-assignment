b948f3506943b7ddfa9b36370858f6d36eea5c1e
XinaHub <tan_aiwei@hotmail.com>
2025-08-07 12:06:07 +0800

    Create methodology.py

---
+++ pages/methodology.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.title("🆕 New Pop-Out Page")
+st.write("This is a separate page in your Streamlit multi-page app.")
+
+# Example: You can reuse components here
+st.write("You can use your existing components here too!")
