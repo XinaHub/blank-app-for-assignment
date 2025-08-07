e1b455ed0953ac577c8a1774dfe2a684aacd11ca
XinaHub <tan_aiwei@hotmail.com>
2025-08-07 12:08:49 +0800

    Delete pages directory

--- pages/methodology.py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-st.title("🆕 New Pop-Out Page")
-st.write("This is a separate page in your Streamlit multi-page app.")
-
-# Example: You can reuse components here
-st.write("You can use your existing components here too!")
