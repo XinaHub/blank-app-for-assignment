ccf398b1a18d5eb161f6d2bdad1dd0e6de1c4e54
XinaHub <tan_aiwei@hotmail.com>
2025-08-07 13:07:23 +0800

    Update methodology.py

--- pages/methodology.py
+++ pages/methodology.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from auth import check_password  # 👈 import your password checker
+
+if not check_password():
+    st.stop()
 
 st.set_page_config(page_title="Analysis", page_icon="📊")
 
